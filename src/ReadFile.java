4a0a0ad3a8220cfeb631e732ce2512db07a341d9
Guilherme <guilherme.s.barros@outlook.com>
2025-08-05 20:18:13 -0300

    read file

---
+++ src/ReadFile.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        final var filePath = "test.txt";
+
+        try (var reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("Could not locate file location.");
+        } catch (IOException exception) {
+            System.out.println("Could not read the file.");
+        }
+    }
+}

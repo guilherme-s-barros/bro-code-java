a7e9e9dda2b3c9d79a8daa59e347070ce28a7ca2
Guilherme <guilherme.s.barros@outlook.com>
2025-08-07 21:33:49 -0300

    enums

---
+++ src/enums/Program.java
@@ -0,0 +1,23 @@
+package enums;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        try (var scanner = new Scanner(System.in)) {
+            System.out.print("Enter a day of the week: ");
+            var response = scanner.nextLine().toUpperCase();
+
+            var day = Day.valueOf(response);
+
+            switch (day) {
+                case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY ->
+                        System.out.println("It is a weekday");
+                case SATURDAY, SUNDAY ->
+                        System.out.println("It is the weekend");
+            }
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Please enter a valid day.");
+        }
+    }
+}
